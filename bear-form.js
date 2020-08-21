fc36581bab3dc2719803c52a1f60e532cb546d1a
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-08-20 21:29:03 -0500

    added bear array, print to dom, and new js files

---
+++ bear-form.js
@@ -0,0 +1,11 @@
+const bearCard = [
+    {
+        bear: "Sun Bear",
+        imgUrl: "https://www.theanimalspot.com/wp-content/uploads/2019/01/sunbearsmall.jpg",
+    }
+];
+
+const whatBear = () => {
+    const bear = document.getElementById("bearInfo").value;
+    bearCard.push({ bear: info, imgUrl: img });
+}
